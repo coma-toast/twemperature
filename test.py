3dd8d566dafe08aea9ab21190a57176ad736101c
Jason Dale <jdale215@gmail.com>
2018-07-19 20:33:13 +0000

    fixed some logging issues

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import Adafruit_DHT as dht
 from time import sleep
 
+#This tests to make sure the sensor is accessible and returning data. Every 5 seconds. 
 while True:
     h,t = dht.read_retry(dht.DHT22, 4)
     f = format((9.0/5.0 * t + 32), '.1f')
